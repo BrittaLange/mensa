4b7e9edd375f3a2b2842421899451dcbcab73784
Britta Lange <britta.lange@mailbox.org>
2021-05-22 07:19:46 +0200

    changed structure of project

---
+++ src/app/components/menu-list/menu-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-list',
+  templateUrl: './menu-list.component.html',
+  styleUrls: ['./menu-list.component.css']
+})
+export class MenuListComponent implements OnInit {
+
+  title = "Menüliste";
+
+  meals = [
+    {"meal": "Essen 1", "name": "Pommes frites", "price": "2.00" },
+    {"meal": "Essen 2", "name": "Hähnchenschnitzel mit Champignonsauce und Pommes frites", "price": "5.60" },
+    {"meal": "Essen 3", "name": "Currywurst mit Currysauce 1997 classic und Pommes frites", "price": "5.60" },
+    {"meal": "Essen 4", "name": "Gebackenes Seelachsfilet mit Remoulade und Kartoffelsalat", "price": "5.80" }
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
