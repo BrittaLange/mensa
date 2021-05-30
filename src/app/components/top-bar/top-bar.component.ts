8a2a5feb742e12816d7e07dfc53e54aa5dc04c77
Britta Lange <britta.lange@mailbox.org>
2021-05-30 07:41:42 +0200

    Added styling and setLocation-method

--- src/app/components/top-bar/top-bar.component.ts
+++ src/app/components/top-bar/top-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Locus } from 'src/app/model/locus';
+import { MensaService } from 'src/app/services/mensa.service';
 
 @Component({
   selector: 'app-top-bar',
@@ -7,11 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopBarComponent implements OnInit {
 
-  location: string = "Cafeteria Mensa 2";
+  public locCafeMensa2: string = "Cafeteria Mensa 2";
+  public locMensa1: string = "Mensa 1";
+  public locMensa2: string = "Mensa 2";
 
-  constructor() { }
+  public location: string  = "Cafeteria Mensa 2";
+
+  DateObj = new Date();
+  today = this.DateObj.getFullYear() + '-' + ('0' + (this.DateObj.getMonth() + 1)).slice(-2) + '-' + ('0' + this.DateObj.getDate()).slice(-2);
+
+  // ToDo: Locus-Objekt um Ort und Zeit an Service zu senden
+  mensa1: Locus = new Locus(101, 'Mensa 1');
+  mensa2: Locus = new Locus(105, 'Mensa 2');
+  cafeMensa2: Locus = new Locus(106, 'Cafeteria Mensa 2');
+
+  constructor(private service: MensaService) {
+
+   }
 
   ngOnInit(): void {
+    this.service.getLocation(this.location);
+  }
+
+  setLocation(loc:string) {
+    this.location = loc;
+    this.service.getLocation(this.location);
+    console.log(this.location);
   }
 
 }
