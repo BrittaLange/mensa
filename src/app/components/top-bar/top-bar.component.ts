81534d4f251421a5a4dad29f8c344bb277022982
Britta Lange <britta.lange@mailbox.org>
2021-06-03 19:02:16 +0200

    Buttons change styling when clicked

--- src/app/components/top-bar/top-bar.component.ts
+++ src/app/components/top-bar/top-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Locus } from 'src/app/model/locus';
 import { MensaService } from 'src/app/services/mensa.service';
 import { UrlService } from 'src/app/services/url.service';
@@ -16,12 +17,21 @@ export class TopBarComponent implements OnInit {
   mensa2: Locus = new Locus(105, 'Mensa 2');
   cafeMensa2: Locus = new Locus(106, 'Cafeteria Mensa 2');
 
-  public location: Locus  = this.cafeMensa2;
+  // When starting the app, first location shown is Cafeteria Mensa 2
+  location: Locus  = this.cafeMensa2;
+
+  statusMensa1: boolean;
+  statusMensa2: boolean;
+  statusCafeMensa2: boolean;
 
   DateObj = new Date();
   today = this.DateObj.getFullYear() + '-' + ('0' + (this.DateObj.getMonth() + 1)).slice(-2) + '-' + ('0' + this.DateObj.getDate()).slice(-2);
 
   constructor(private urlservice: UrlService, private service: MensaService) {
+    this.location = this.cafeMensa2;
+    this.statusMensa1 = false;
+    this.statusMensa2 = false;
+    this.statusCafeMensa2 = true;
    }
 
   ngOnInit(): void {
@@ -29,9 +39,28 @@ export class TopBarComponent implements OnInit {
   }
 
   setLocation(loc:Locus) {
+    this.setButtonColor(loc);
     this.location = loc;
     this.urlservice.getLocation(this.location);
     console.log(this.location);
     this.service.getData();
+  }
+
+  setButtonColor(loc:Locus) {
+    if(loc.id == 101){
+      this.statusMensa1 = true;
+      this.statusMensa2 = false;
+      this.statusCafeMensa2 = false;
+    }
+    if(loc.id == 105){
+      this.statusMensa1 = false;
+      this.statusMensa2 = true;
+      this.statusCafeMensa2 = false;
+    }
+    if(loc.id == 106){
+      this.statusMensa1 = false;
+      this.statusMensa2 = false;
+      this.statusCafeMensa2 = true;
+    }
   }
 }
